e24699300ac89b789321d616c2306491aaef124f
钟佩颖 <211987006@qq.com>
2023-05-04 20:11:43 +0800

    666

---
+++ 晨/1.c
@@ -0,0 +1,77 @@
+/*#include <stdio.h>
+int main()
+{
+	printf("hahha\n");
+	return 0;
+}*/
+/*
+	char 字符类型
+	int 整型
+	short 短整型
+	long 长整型
+	long long 更长的整型
+	float 单精度浮点型
+	double 双精度浮点型
+
+*/
+/*
+#include <stdio.h>
+int main()
+{
+	printf("yoona\n");
+	printf("%d\n",100);//打印一个整数 
+	//sizeof 关键字 操作符 计算类型或变量所占空间的大小     单位是字节      一字节八比特
+	printf("%d\n", sizeof(char));//1
+	printf("%d\n", sizeof(int));//4
+	printf("%d\n", sizeof(short));//2
+	printf("%d\n", sizeof(long));//4
+	printf("%d\n", sizeof(long long));//8
+	printf("%d\n", sizeof(double));//8
+	printf("%d\n", sizeof(float));//4
+	return 0;
+}
+*/
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 20;
+//	double weight = 75.3;
+//	age = age + 1;
+//	weight = weight - 1;
+//	printf("%d\n",age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int a = 100;//全局变量 {}外部定义的
+//int main()
+//{
+//	int a = 10;//局部变量 {}内部定义的
+//	//当局部变量和全局变量名字冲突时，局部变量优先
+//	printf("%d\n", a);
+//	
+//	return 0;
+//}
+
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf_s("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+
+
+	return 0;
+}
